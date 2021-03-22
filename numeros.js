788f4cd3f83d41c2ebf2ad0152fb0aad8d2cb4b6
Juan Pablo <a329688@uach.mx>
2021-03-22 00:31:46 -0600

    add all actividades

---
+++ numeros.js
@@ -0,0 +1,14 @@
+for(let i =1;i <=10;i++){
+    console.log(i);
+    if(i==5)
+        alert("buenas");
+}
+
+function suma(){
+    console.log("la suma es" + (2+3));
+}
+suma();
+
+const suma2 = (a,b) => 2+3;
+
+suma2(2,5);
